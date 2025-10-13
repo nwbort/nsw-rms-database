56472542eca93b6c32090ae8d32e9a63076e5ab4
nwbort <30829548+nwbort@users.noreply.github.com>
2025-10-13 11:45:38 +1100

    Update scrape.sh

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 ./download.sh 'https://www.livetraffic.com/traffic/hazards/roadwork.json'
+./download.sh 'https://www.livetraffic.com/traffic/hazards/incident.json'
+./download.sh 'https://www.livetraffic.com/traffic/hazards/flood.json'
+./download.sh 'https://www.livetraffic.com/traffic/hazards/alpine.json'
+./download.sh 'https://www.livetraffic.com/traffic/hazards/fire.json'
+./download.sh 'https://www.livetraffic.com/traffic/hazards/majorevent.json'
+./download.sh 'https://www.livetraffic.com/traffic/hazards/lga-incidents.json'
